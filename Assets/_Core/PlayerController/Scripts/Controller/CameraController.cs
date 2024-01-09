0af137817b44014fdec1d70135399782a8ba1c9c
Logan <pix.59700@gmail.com>
2024-01-09 15:37:25 +0100

    PlayerMovement go to fodward and right

--- Assets/_Core/PlayerController/Scripts/Controller/CameraController.cs
+++ Assets/_Core/PlayerController/Scripts/Controller/CameraController.cs
@@ -16,7 +16,6 @@ public class CameraController : MonoBehaviour
     {
         // The camera takes the position of the movement, and the movement takes the rotation of the camera.
         transform.position = _movementController.transform.position;
-        _movementController.transform.rotation = Quaternion.Euler(_movementController.transform.rotation.x, transform.rotation.y, _movementController.transform.rotation.z);
 
         _rotationY -= _axisMovement.x * _sensibility;
         _rotationX += _axisMovement.y * _sensibility;
@@ -24,11 +23,11 @@ public class CameraController : MonoBehaviour
         _rotationX = Mathf.Clamp(_rotationX, -_cameraClamp, _cameraClamp);
         
         transform.rotation = Quaternion.Euler(-_rotationX, -_rotationY, 0);
+        _movementController.transform.rotation = Quaternion.Euler(0, -_rotationY, 0);
     }
 
     public void SetAxisMovement(Vector2 axis)
     {
-        Debug.Log(axis);
         _axisMovement = axis;
     }
 }
