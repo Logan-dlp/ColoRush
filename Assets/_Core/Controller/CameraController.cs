8bccd2f098882430dd56a648df812c8bbe257cb9
Logan <pix.59700@gmail.com>
2024-01-10 12:43:07 +0100

    Change for camera is children of the player

--- Assets/_Core/Controller/CameraController.cs
+++ Assets/_Core/Controller/CameraController.cs
@@ -7,24 +7,19 @@ public class CameraController : MonoBehaviour
     private float _rotationY;
     private float _rotationX;
     
-    [SerializeField] private Transform _targetFollowPosition;
-    [SerializeField] private Vector3 _cameraPosition;
-    
     [SerializeField] private float _cameraClamp;
     [SerializeField] private float _sensibility;
 
-    private void LateUpdate()
+    private void Update()
     {
         // The camera takes the position of the movement, and the movement takes the rotation Y of the camera.
-        transform.position = _targetFollowPosition.position + _cameraPosition;
-
         _rotationY -= _axisMovement.x * _sensibility;
         _rotationX += _axisMovement.y * _sensibility;
 
         _rotationX = Mathf.Clamp(_rotationX, -_cameraClamp, _cameraClamp);
         
-        transform.rotation = Quaternion.Euler(-_rotationX, -_rotationY, 0);
-        _targetFollowPosition.rotation = Quaternion.Euler(0, -_rotationY, 0);
+        transform.parent.rotation = Quaternion.Euler(0, -_rotationY, 0);
+        transform.localRotation = Quaternion.Euler(-_rotationX, 0, 0);
     }
 
     public void SetAxisMovement(Vector2 axis)
