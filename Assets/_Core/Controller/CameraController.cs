97d048a7d2abe1d6d248991bfd1f39d37d904581
Logan <pix.59700@gmail.com>
2024-01-10 12:24:28 +0100

    Change nomenclature

--- Assets/_Core/Controller/CameraController.cs
+++ Assets/_Core/Controller/CameraController.cs
@@ -7,16 +7,16 @@ public class CameraController : MonoBehaviour
     private float _rotationY;
     private float _rotationX;
     
-    [SerializeField] private MovementController _movementController;
-    
+    [SerializeField] private Transform _targetFollowPosition;
     [SerializeField] private Vector3 _cameraPosition;
+    
     [SerializeField] private float _cameraClamp;
     [SerializeField] private float _sensibility;
 
     private void LateUpdate()
     {
         // The camera takes the position of the movement, and the movement takes the rotation Y of the camera.
-        transform.position = _movementController.transform.position + _cameraPosition;
+        transform.position = _targetFollowPosition.position + _cameraPosition;
 
         _rotationY -= _axisMovement.x * _sensibility;
         _rotationX += _axisMovement.y * _sensibility;
@@ -24,7 +24,7 @@ public class CameraController : MonoBehaviour
         _rotationX = Mathf.Clamp(_rotationX, -_cameraClamp, _cameraClamp);
         
         transform.rotation = Quaternion.Euler(-_rotationX, -_rotationY, 0);
-        _movementController.transform.rotation = Quaternion.Euler(0, -_rotationY, 0);
+        _targetFollowPosition.rotation = Quaternion.Euler(0, -_rotationY, 0);
     }
 
     public void SetAxisMovement(Vector2 axis)
